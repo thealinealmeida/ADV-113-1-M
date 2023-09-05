837e703ce7f1b41d2070c3c75ea50b6acd0440bd
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2023-09-05 19:03:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const camera = document.getElementById("camera");
+const result = document.getElementById("result");
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+
+Webcam.attach(camera);
+
+function snap() {
+    result.innerHTML = "";
+    Webcam.snap(function (dataURI) {
+        const captura = document.createElement("img");
+        captura.setAttribute("id", "captura");
+        captura.setAttribute("src", dataURI);
+        result.appendChild(captura);
+    });
+}
+//const teachablemachineLink = ;
+const modelLink = teachablemachineLink + 'model.json'
+const classifier = ml5.imageClassifier(modelLink, modelLoaded)
+
