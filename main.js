5ac9ed93c68eadb9e69a18437b1b4bfce5911b3c
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2023-09-05 21:13:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -23,27 +23,3 @@ const teachablemachineLink = '';
 const modelLink = teachablemachineLink + 'model.json'
 const classifier = ml5.imageClassifier(modelLink, modelLoaded)
 
-function modelLoaded(){
-    console.log('Modelo Carregado')
-    Webcam.attach(camera);
-
-}
-function check() {
-    const img = document.getElementById('captura');
-    classifier.classify(img, gotResult);
-}
-function gotResult(error, results) {
-    console.log(error);
-    console.log(results);
-
-    const objeto = document.getElementById("objectName");
-    const precisao = document.getElementById("objectAccuracy");
-
-    if (!error) {
-        objeto.innerHTML = results[0].label;
-        precisao.innerHTML = results[0].confidence.toFixed(2);
-    } else {
-        console.error(error);
-    }
-}
-
